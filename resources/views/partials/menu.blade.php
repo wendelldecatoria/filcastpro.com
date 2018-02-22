3b9fed3252a0846d8065d47ad150a56986d39e31
Wendell Decatoria <wendell.t.decatoria@gmail.com>
2018-02-23 00:44:27 +0800

    dev commit

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -12,11 +12,18 @@
     <!-- <div class="navbar-header">
       <a class="navbar-brand" href="#"></a>
     </div> -->
-    <ul class="nav navbar-nav">
-      <li class="nav-home"><a href="{{route('web.home')}}">HOME</a></li>
-      <li class="nav-actors"><a href="{{route('web.actors')}}">ACTORS</a></li>
-      <li class="nav-news-room"><a href="{{route('web.news-room')}}">NEWS ROOM</a></li>
-      <li class="nav-contact"><a href="{{route('web.contact')}}">CONTACT</a></li>
-    </ul>
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+    </button>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li class="nav-home"><a href="{{route('web.home')}}">HOME</a></li>
+          <li class="nav-actors"><a href="{{route('web.actors')}}">ACTORS</a></li>
+          <li class="nav-news-room"><a href="{{route('web.news-room')}}">NEWS ROOM</a></li>
+          <li class="nav-contact"><a href="{{route('web.contact')}}">CONTACT</a></li>
+        </ul>
+    </div>
   </div>
 </nav>
