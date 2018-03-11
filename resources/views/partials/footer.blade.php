7727b0c0c0eb8a4bc23e570772536b6b5aa2f1ea
Wendell Decatoria <wendell.t.decatoria@gmail.com>
2018-03-11 22:10:30 +0800

    removed T in filcaspro footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 
 
 <div class="row footer">
-    <a href="http://joweemorel.com" target="_blank" style="color:#fff;">&copy; 2018 Filcastpro.com. All Rights Reserved. Powered by: Jowee Morel</a>      
+    <a href="http://joweemorel.com" target="_blank" style="color:#fff;">&copy; 2018 Filcaspro.com. All Rights Reserved. Powered by: Jowee Morel</a>      
 </div>
