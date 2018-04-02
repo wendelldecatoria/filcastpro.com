9527037e4fead93456b044f442c345102ba55237
Wendell Decatoria <wendell.t.decatoria@gmail.com>
2018-04-02 16:19:44 +0800

    dev commit

--- resources/views/whats-up/show.blade.php
+++ resources/views/whats-up/show.blade.php
@@ -13,7 +13,7 @@
         <div class="col-md-8">
         <a href="{{route('web.whats-up')}}"><button type="button" class="btn btn-default">see all articles</button></a>
             <!-- <div class="col-md-12"> <img src="{{-- asset('/storage/images/writers/'. $article->writer->image ) --}}" width="250px" /></div> -->
-            <h2>{{$article[0]->writer->title}} by {{$article[0]->writer->name}} </h2>
+            <h2>{{$article[0]->title}} by {{$article[0]->writer}} </h2>
             <h3>{{$article[0]->headline}}</h3>
             <hr class="hr">
             <p> {!! htmlspecialchars_decode($article[0]->content) !!}</p>
