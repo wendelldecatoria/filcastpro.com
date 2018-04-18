e65f0404a1de6b736e79694d22e3c821a20b1e24
wendelldecatoria <wendell@onsite.com.ph>
2018-04-19 01:08:20 +0800

    website changes

--- app/Http/Controllers/WebWhatsOnController.php
+++ app/Http/Controllers/WebWhatsOnController.php
@@ -15,9 +15,25 @@ class WebWhatsOnController extends Controller
     *
     */
     public function index(){
-        $today = WhatsOn::whereDate('date_from','=', Carbon::today()->toDateString() )->get();
-        //return view('web.whats-on.index');
-        return $today;
+        $dt = Carbon::now();
+        $today = WhatsOn::whereDate('date_from','=', Carbon::today()->toDateString())->orderBy('date_from')->get();
+
+        if($dt->isSaturday() ){ //if Carbon::now() is a saturday
+            // get all records from today until end of week saturday
+            $thisWeek = WhatsOn::whereDate('date_from','=', Carbon::today()->toDateString())->orderBy('date_from')->get();
+        }else{
+            // get all records today until Saturday
+            $thisWeek = WhatsOn::whereBetween('date_from', [ Carbon::today()->toDateString() ,  Carbon::parse('this saturday')->toDateString() ])->orderBy('date_from')->get();
+        }
+
+        $nextWeek = WhatsOn::whereBetween('date_from', [ Carbon::parse('this sunday')->toDateString() ,  Carbon::parse('this saturday')->addWeek()->toDateString() ])->orderBy('date_from')->get();
+
+
+        $thisMonth = WhatsOn::whereMonth('date_from', '=', date('m') )->orderBy('date_from')->get();
+        $nextMonth = WhatsOn::whereMonth('date_from', '>', date('m') )->orderBy('date_from')->get();
+
+        return view('web.whats-on.index', compact('today','thisWeek','nextWeek', 'thisMonth', 'nextMonth') );
+        // return $thisWeek;
     }
 
     /*
--- resources/views/web/whats-on/index.blade.php
+++ resources/views/web/whats-on/index.blade.php
@@ -17,11 +17,65 @@
               </div>
           </div>
           <div class="row">
-            <div class="col-md-2 col-md-offset-1"><h3 class="event-title">Today</h3></div>
-            <div class="col-md-2"><h3 class="event-title">This Week</h3></div>
-            <div class="col-md-2"><h3 class="event-title">Next Week</h3></div>
-            <div class="col-md-2"><h3 class="event-title">This Month</h3></div>
-            <div class="col-md-2"><h3 class="event-title">Next Month</h3></div>
+            <div class="col-md-2 col-md-offset-1 col-event">
+              <h3 class="event-header">Today</h3>
+              @foreach($today as $event)
+              <div class="row">
+                  @if(!empty($event->url))<a href="{{ $event->url }}" target="_blank"> @endif
+                    <p class="event-title"> {{ $event->title }}</p>
+                  @if(!empty($event->url))</a> @endif
+                  <p>{{ date('h:i a', strtotime($event->date_from)) }}, {{ $event->venue }}</p>
+              </div>
+              @endforeach
+            </div>
+            <div class="col-md-2 col-event">
+              <h3 class="event-header">This Week</h3>
+
+              @foreach($thisWeek as $event)
+                <div class="row">
+                    @if(!empty($event->url))<a href="{{ $event->url }}" target="_blank"> @endif
+                      <p class="event-title"> {{ $event->title }}</p>
+                    @if(!empty($event->url))</a> @endif
+                    <p>{{ date('l', strtotime($event->date_from)) }} , {{ date('h:i a', strtotime($event->date_from)) }}, {{ $event->venue }}</p>
+                </div>
+              @endforeach
+            </div>
+            <div class="col-md-2 col-event">
+              <h3 class="event-header">Next Week</h3>
+
+              @foreach($nextWeek as $event)
+                <div class="row">
+                    @if(!empty($event->url))<a href="{{ $event->url }}" target="_blank"> @endif
+                      <p class="event-title"> {{ $event->title }}</p>
+                    @if(!empty($event->url))</a> @endif
+                    <p>{{ date('l', strtotime($event->date_from)) }} , {{ date('h:i a', strtotime($event->date_from)) }}, {{ $event->venue }}</p>
+                </div>
+              @endforeach
+            </div>
+            <div class="col-md-2 col-event">
+              <h3 class="event-header">This Month</h3>
+
+              @foreach($thisMonth as $event)
+                <div class="row">
+                    @if(!empty($event->url))<a href="{{ $event->url }}" target="_blank"> @endif
+                      <p class="event-title"> {{ $event->title }}</p>
+                    @if(!empty($event->url))</a> @endif
+                    <p>{{ date('l', strtotime($event->date_from)) }} , {{ date('h:i a', strtotime($event->date_from)) }}, {{ $event->venue }}</p>
+                </div>
+              @endforeach
+            </div>
+            <div class="col-md-2 col-event">
+              <h3 class="event-header">Next Month</h3>
+
+              @foreach($nextMonth as $event)
+                <div class="row">
+                    @if(!empty($event->url))<a href="{{ $event->url }}" target="_blank"> @endif
+                      <p class="event-title"> {{ $event->title }}</p>
+                    @if(!empty($event->url))</a> @endif
+                    <p>{{ date('l', strtotime($event->date_from)) }} , {{ date('h:i a', strtotime($event->date_from)) }}, {{ $event->venue }}</p>
+                </div>
+              @endforeach
+            </div>
         </div>
         </div>
         <div class="col-md-1"></div>
