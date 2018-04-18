c3961a92d30cee816dc60689391547d59df9d7f4
wendelldecatoria <wendell@onsite.com.ph>
2018-04-19 01:13:18 +0800

    disable whats in

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -6,7 +6,7 @@
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
+        <span class="icon-bar"></span>
     </button>
     <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav">
@@ -15,7 +15,7 @@
           <li class="nav-creatives"><a href="{{route('web.creative.index')}}">CREATIVES</a></li>
           <li class="nav-whats-up"><a href="{{route('web.whats-up.index')}}">WHAT'S UP</a></li>
           <li class="nav-whats-on"><a href="{{route('web.whats-on.index')}}">WHAT'S ON</a></li>
-          <li class="nav-whats-in"><a href="{{route('web.whats-in.index')}}">WHAT'S IN</a></li>   
+          <!-- <li class="nav-whats-in"><a href="{{route('web.whats-in.index')}}">WHAT'S IN</a></li>    -->
           <li class="nav-register"><a href="{{route('web.register.index')}}">REGISTER</a></li>
           <li class="nav-contact"><a href="{{route('web.contact.index')}}">CONTACT US</a></li>
         </ul>
