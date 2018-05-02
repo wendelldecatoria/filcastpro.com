48ca9a2768dc18f6536cb417fb61b4b91dcd167a
Wendell Decatoria <wendell.t.decatoria@gmail.com>
2018-05-02 12:27:54 +0800

    facebook comment on whats up page

--- resources/views/web/whats-up/show.blade.php
+++ resources/views/web/whats-up/show.blade.php
@@ -11,12 +11,14 @@
         @include('partials.menu')
         <div class="col-md-2"></div>
         <div class="col-md-8">
-        <a href="{{route('web.whats-up.index')}}"><button type="button" class="btn btn-default">see all articles</button></a>
+            <a href="{{route('web.whats-up.index')}}"><button type="button" class="btn btn-default">see all articles</button></a>
            <!--  <div class="col-md-12"> <img src="{{-- asset('/storage/images/writers/'. $article[0]->image ) --}}" class="img-thumbnail" width="180" height="180" /></div> -->
             <h3 id="headline"><strong>{{$article[0]->headline}}</strong></h3>
             <h5 id="title">{{$article[0]->title}} by {{$article[0]->writer}} </h5>
             <hr class="hr">
             <p class="text-justify"> <blockquote>{!! htmlspecialchars_decode($article[0]->content) !!} </blockquote></p>
+
+            <div class="fb-comments" data-href="{{route('web.whats-up.show', $article[0]->id)}}" data-width="100%" data-numposts="5"></div>
         </div>
         <div class="col-md-2"></div>
     </div>
